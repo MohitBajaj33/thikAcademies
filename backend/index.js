5c0160cacfec6fc219745065fabb972399ee6dea
Mohit <mohitbajaj89955@gmail.com>
2025-05-10 18:01:28 +0530

    add default route

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,10 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+
+app.get("/",(req,res)=>{
+  res.send("Backend is working properly")
+})
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/attempt', attemptRoutes);
@@ -31,4 +35,4 @@ const connectDB = require('./config/db');
 connectDB();
  
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
